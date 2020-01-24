767abaca7fed66adbcd16a78c54f9371786bd3eb
Nitin Chaurasia <gs.nitin@gmail.com>
2020-01-24 16:23:45 -0500

    spring batch added

---
+++ src/main/java/com/learning/batchprocessing/User.java
@@ -0,0 +1,16 @@
+package com.learning.batchprocessing;
+
+import lombok.*;
+
+/**
+ * Created by nichaurasia on Friday, January/24/2020 at 2:35 PM
+ */
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private String lastName;
+    private String firstName;
+}
