c0d30024fbfd7c36d2953a8134083bc0c5760c04
Nitin Chaurasia <gs.nitin@gmail.com>
2020-02-07 23:38:48 -0500

    Create JobConfiguration.java

---
+++ src/main/java/com/learning/batchprocessing/controller/JobConfiguration.java
@@ -0,0 +1,16 @@
+package com.learning.batchprocessing.controller;
+
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by nitin on Friday, February/07/2020 at 11:37 PM
+ */
+
+@Configuration
+@EnableBatchProcessing
+public class JobConfiguration {
+
+    @JobBuil
+}
