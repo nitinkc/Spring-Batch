07f2c08df7ffc9f4a2817204abce41bead7a6324
Nitin Chaurasia <gs.nitin@gmail.com>
2020-02-08 01:20:00 -0500

    Update JobConfiguration.java

--- src/main/java/com/learning/batchprocessing/controller/JobConfiguration.java
+++ src/main/java/com/learning/batchprocessing/controller/JobConfiguration.java
@@ -1,7 +1,17 @@
 package com.learning.batchprocessing.controller;
 
 import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -12,5 +22,31 @@ import org.springframework.context.annotation.Configuration;
 @EnableBatchProcessing
 public class JobConfiguration {
 
-    @JobBuil
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Step step100(){
+        return stepBuilderFactory.get("step1")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println("Hello World!");
+                        return RepeatStatus.FINISHED;
+                    }
+                }).build();
+    }
+
+    //Add the step into a job
+
+    @Bean
+    public Job helloWorldJob(){
+        return jobBuilderFactory.get("helloWorldJob")
+                .start(step100())
+                .build();
+    }
+
 }
