57c8c688a738bf98809cf427e611671007d7bada
Nitin Chaurasia <gs.nitin@gmail.com>
2020-02-08 21:14:22 -0500

    Create S1HelloWorldConfig.java

---
+++ src/main/java/com/learning/batchprocessing/batchConfig/S1HelloWorldConfig.java
@@ -0,0 +1,50 @@
+package com.learning.batchprocessing.batchConfig;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by nitin on Saturday, February/08/2020 at 9:06 PM
+ */
+@Configuration
+@EnableBatchProcessing
+public class S1HelloWorldConfig {
+
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Step step100(){
+        return stepBuilderFactory.get("step1")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println("Hello World!");
+                        return RepeatStatus.FINISHED;
+                    }
+                }).build();
+    }
+
+    //Add the step into a job
+
+    @Bean
+    public Job helloWorldJob(){
+        return jobBuilderFactory.get("helloWorldJob")
+                .start(step100())
+                .build();
+    }
+
+}
